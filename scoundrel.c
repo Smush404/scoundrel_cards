2b91e6dc284d60213b9189edb30b3f170c62e70c
Haakon H <hhaus532@iastate.edu>
2025-12-27 15:18:03 -0600

    added: spliting a deck in half for shuffle

--- scoundrel.c
+++ scoundrel.c
@@ -11,6 +11,7 @@ int value;
 
 Card *deck[MAX_NUM_CARDS];
 
+//creates template deck
 void create_deck(){
   int count = 0;
   char type[] = {'D', 'C', 'S', 'H'};
@@ -20,30 +21,49 @@ void create_deck(){
     deck[i] = malloc(sizeof(Card));
   }
 
+  //fill template deck
   for (int i = 0; i < 4; i++)
   {
     for (int j = 0; j < 13; j++)
     {
       deck[count]->type = type[i];
       deck[count]->value = number[j];
-      printf("i:%d j:%d count:%d %c%d \n", i, j, count, deck[count]->type, deck[count]->value);
+      //printf("i:%d j:%d count:%d %c%d \n", i, j, count, deck[count]->type, deck[count]->value);
       count++;
     }
   }
 
+  //add jokers
   deck[count++]->type = 'J';
   deck[count]->type = 'J';
 
-  for (int i = 0; i < MAX_NUM_CARDS; i++)
-  {
-    printf("%c%d, ", deck[i]->type, deck[i]->value);
-  }
-  
-  
+  //DEBUG
+  // for (int i = 0; i < MAX_NUM_CARDS; i++)
+  // {
+  //   printf("%c%d, ", deck[i]->type, deck[i]->value);
+  // }  
 }
 
+
+//mian shuffler of deck
+Card * shuffle_main(Card *a, Card *b){
+
+  return NULL;
+}
+
+//shuffles cards in the deck
+void shuffle(){
+  Card *deckL[MAX_NUM_CARDS/2];
+  Card *deckR[MAX_NUM_CARDS/2];
+
+  for(int i = 0; i < MAX_NUM_CARDS/2; i++){
+    deckL[i] = deck[i];
+    deckR[i] = deck[i + MAX_NUM_CARDS/2];
+  }
+
+  Card *tmp = shuffle_main(deckL, deckR);
+}
 int main(){
   create_deck();
-
-  
+  shuffle();
 }
